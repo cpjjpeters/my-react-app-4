352cfe35d03e09c73279bed013cf40d795edf766
Carl Peters <31207893+cpjjpeters@users.noreply.github.com>
2024-07-15 11:09:26 +0200

    final version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import Heading from "./Heading.js";
 import Section from "./Section.js";
 
-export default function Page() {
+export default function ProfilePage() {
   return (
     <Section>
-      <Heading level={1}>Title</Heading>
-      <Section>
-        <Heading level={2}>Heading</Heading>
-        <Heading level={2}>Heading</Heading>
-        <Heading level={2}>Heading</Heading>
-        <Section>
-          <Heading level={3}>Sub-heading</Heading>
-          <Heading level={3}>Sub-heading</Heading>
-          <Heading level={3}>Sub-heading</Heading>
-          <Section>
-            <Heading level={4}>Sub-sub-heading</Heading>
-            <Heading level={4}>Sub-sub-heading</Heading>
-            <Heading level={4}>Sub-sub-heading</Heading>
-          </Section>
-        </Section>
-      </Section>
+      <Heading>My Profile</Heading>
+      <Post title="Hello traveller!" body="Read about my adventures." />
+      <AllPosts />
+    </Section>
+  );
+}
+
+function AllPosts() {
+  return (
+    <Section>
+      <Heading>Posts</Heading>
+      <RecentPosts />
+    </Section>
+  );
+}
+
+function RecentPosts() {
+  return (
+    <Section>
+      <Heading>Recent Posts</Heading>
+      <Post title="Flavors of Lisbon" body="...those pastéis de nata!" />
+      <Post title="Buenos Aires in the rhythm of tango" body="I loved it!" />
+    </Section>
+  );
+}
+
+function Post({ title, body }) {
+  return (
+    <Section isFancy={true}>
+      <Heading>{title}</Heading>
+      <p>
+        <i>{body}</i>
+      </p>
     </Section>
   );
 }
